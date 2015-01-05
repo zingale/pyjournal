1697a0f627ca0074d79ca9d33cb7360e40855352
Michael Zingale <michael.zingale@stonybrook.edu>
2015-01-05 10:02:02 -0500

    vi doesn't like piping stdout/stderr

--- shell_util.py
+++ shell_util.py
@@ -5,8 +5,16 @@ def run(string):
 
     # shlex.split will preserve inner quotes
     prog = shlex.split(string)
-    
-    p0 = subprocess.Popen(prog, stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE)
-    stdout0, stderr0 = p0.communicate()
-    return stdout0, stderr0, p0.returncode
+    if prog[0] == "vi":
+        # vi hangs when piping stdout/stderr
+        p0 = subprocess.Popen(prog)
+        stdout0, stderr0 = p0.communicate()
+        rc = p0.returncode
+
+    else:
+        p0 = subprocess.Popen(prog, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
+        stdout0, stderr0 = p0.communicate()
+        rc = p0.returncode
+        
+    return stdout0, stderr0, rc
